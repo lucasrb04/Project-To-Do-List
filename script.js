e971d83d4f5166a76dbaba5d81c5d2321ce9897c
Lucas Ribeiro <lucasrb04@gmail.com>
2021-03-10 16:32:39 -0300

    realizando requisito 7

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+const lista = document.getElementById('lista-tarefas');
 function addItem() {
   const inputTexto = document.querySelector('#texto-tarefa').value;
-  const lista = document.getElementById('lista-tarefas');
   const newItem = document.createElement('li');
   newItem.innerText = inputTexto;
   lista.appendChild(newItem);
@@ -8,3 +8,7 @@ function addItem() {
 }
 const button = document.getElementById('criar-tarefa');
 button.addEventListener('click', addItem);
+
+lista.addEventListener('click', function(event){
+  event.target.style.backgroundColor = ('rgb(128,128,128');
+});
